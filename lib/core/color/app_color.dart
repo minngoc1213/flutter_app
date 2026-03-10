b559d4045f53100edad36c7e4dfd2ae36df8e51d
Nga Nv <nganv.os@ncb-bank.vn>
2026-03-10 17:04:44 +0700

    feat: Add `AppColors` class to define common application colors.

---
+++ lib/core/color/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color colorBlack = Colors.black;
+  static const Color colorWhite = Colors.white;
+  static const Color transparent = Colors.transparent;
+  static const Color pink = Color(0xFFFFC6C9);
+  static const Color sweetPink = Color(0xFFEC888D);
+  static const Color redPink = Color(0xFFFD5D69);
+  static const Color brownPod = Color(0xFF32201C);
+}
